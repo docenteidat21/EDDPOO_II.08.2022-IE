8f795341ac12597e8e49705815e189085e2ee1c8
Docente IDAT Kenny Mallqui <docenteidat21@gmail.com>
2022-04-07 09:34:48 -0500

    Segunda versión de todo el proyecto EDDPOO  # Please enter the commit message for your changes. Lines starting

--- PROYECTO/Controlador/Empleados.py
+++ PROYECTO/Controlador/Empleados.py
@@ -1,10 +1,47 @@
-class Empleado:
+class Cliente:
 
-    def __init__(self, dniEmpleado, nombresEmpleado, apellidoPaternoEmpleado, apellidoMaternoEmpleado, direccionEmpleado, telefonoEmpleado):
+    def __init__(self, dniCliente, nombresCliente, apellidoPaternoCliente, 
+                 apellidoMaternoCliente, direccionCliente, telefonoCliente):
+        self.__dniCliente = dniCliente 
+        self.__nombresCliente = nombresCliente
+        self.__apellidoPaternoCliente = apellidoPaternoCliente
+        self.__apellidoMaternoCliente = apellidoMaternoCliente
+        self.__direccionCliente = direccionCliente
+        self.__telefonoCliente = telefonoCliente
+    
+    def getDniCliente(self):
+        return self.__dniCliente
 
-        self.dniEmpleado = dniEmpleado 
-        self.nombresEmpleado = nombresEmpleado
-        self.apellidoPaternoEmpleado = apellidoPaternoEmpleado
-        self.apellidoMaternoEmpleado = apellidoMaternoEmpleado
-        self.direccionEmpleado = direccionEmpleado
-        self.telefonoEmpleado = telefonoEmpleado
+    def getNombresCliente(self):
+        return self.__nombresCliente
+
+
+    def getApellidoPaternoCliente(self):
+        return self.__apellidoPaternoCliente
+
+    def getApellidoMaternoCliente(self):
+        return self.__apellidoMaternoCliente
+
+    def getDireccionCliente(self):
+        return self.__direccionCliente
+
+    def getTelefonoCliente(self):
+        return self.__telefonoCliente
+
+    def setDniCliente(self, dniCliente):
+        self.__dniCliente = dniCliente
+
+    def setNombresCliente(self, nombresCliente):
+        self.__nombresCliente = nombresCliente
+    
+    def setApellidoPaternoCliente(self, apellidoPaternoCliente):
+        self.__apellidoPaternoCliente = apellidoPaternoCliente
+
+    def setApellidoMaternoCliente(self, apellidoMaternoCliente):
+        self.__apellidoMaternoCliente = apellidoMaternoCliente
+
+    def setDireccionCliente(self, direccionCliente):
+        self.__direccionCliente = direccionCliente
+    
+    def setTelefonoCliente(self, telefonoCliente):
+        self.__telefonoCliente = telefonoCliente
